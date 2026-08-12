593393b675a6411d696ca1c87d2d4cea3a17c0e6
Lavanya <lavanyachhabra33@gmail.com>
2026-08-12 14:46:39 +0530

    seed funding updates

--- src/pages/SeedFundingSections/SeedMain.jsx
+++ src/pages/SeedFundingSections/SeedMain.jsx
@@ -34,7 +34,7 @@ useScrollAnimation();
         <div className="hero-content reveal slide-left">
           <div className="badge">
             <span className="dot"></span>
-            <h2>Completion Activities Update</h2>
+            <h2>Funded Teams Showcase Update</h2>
           </div>
 
           <h1>
